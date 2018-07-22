b59c1dc94927a45a57c0edf1b26f9d197368c6c3
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-22 19:17:46 +0200

    Zadania domowe z zajęć nr 5. - poprawiona wersja 5.7//names

--- src/com/example/homework/app/ZadanieJava14.java
+++ src/com/example/homework/app/ZadanieJava14.java
@@ -12,29 +12,18 @@ public class ZadanieJava14 {
         System.out.println("Wpisz 5 imion");
         Scanner scanner = new Scanner(System.in);
         String[] name14s = new String[5];
-        String imie = scanner.nextLine();
-        name14s[0] = imie;
-        imie = scanner.nextLine();
-        name14s[1] = imie;
-        imie = scanner.nextLine();
-        name14s[2] = imie;
-        imie = scanner.nextLine();
-        name14s[3] = imie;
-        imie = scanner.nextLine();
-        name14s[4] = imie;
+        int i;
 
 
+        for (i = 0; i < name14s.length; i++) {
+            name14s[i] = scanner.nextLine();
 
-        String odwrotne[] = new String[name14s.length];
+        }
 
-        for (int i = name14s.length - 1; i > 0;) {
+        for (i = name14s.length - 1; i >= 0; i--) {
 
-            for (int j = 0; j < name14s.length; j++) {
 
-                odwrotne[j] = name14s[i];
-                i--;
-
-                System.out.println("Cześć " + odwrotne[j]);
+                System.out.println("Cześć " + name14s[i]);
             }
 
 
@@ -44,4 +33,4 @@ public class ZadanieJava14 {
 
     }
 
-}
+
