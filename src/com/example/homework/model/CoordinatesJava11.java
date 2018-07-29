dd96645ae8e80906752de73d21a4a65333aa9d78
diabolique79 <m.mielnicka79@gmail.com>
2018-07-29 20:24:25 +0200

    Zadania domowe z zajęć nr 5. - poprawiona wersja 5.4// - usunięcie niepotrzebnych pól

--- src/com/example/homework/model/CoordinatesJava11.java
+++ src/com/example/homework/model/CoordinatesJava11.java
@@ -2,17 +2,15 @@ package com.example.homework.model;
 
 public class CoordinatesJava11 {
 
-    private double coordinateX;
-    private double coordinateY;
 
-    public CoordinatesJava11(double coordinateX, double coordinateY) {
-        this.coordinateX = coordinateX;
-        this.coordinateY = coordinateY;
-    }
+    public void CoordinateSystem(double coordinateX, double coordinateY){
 
-    public void CoordinateSystem(){
+        if (coordinateX == 0 && coordinateY == 0){
 
-        if(coordinateX > 0 && coordinateY > 0){
+            System.out.println("Srodek układu kartezjańskiego.");
+        }
+
+        else if(coordinateX > 0 && coordinateY > 0){
 
             System.out.println("Punkt leży w pierwszej ćwiartce.");
         }
@@ -31,11 +29,8 @@ public class CoordinatesJava11 {
             System.out.println("Punkt leży w czwartej ćwiartce.");
         }
 
-        else {
 
-            System.out.println("Srodek układu kartezjańskiego.");
-        }
 
     }
-    
+
 }
